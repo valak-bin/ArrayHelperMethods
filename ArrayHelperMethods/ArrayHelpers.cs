e5f26357c8ca8ee472532bf0d724c3ca5ed8e6b7
jered <me@jered.dev>
2022-07-16 16:17:26 -0600

    Create ArrayHelpers class, SwapInts methods.
    Create the class ArrayHelpers to hold all of the helper methods. Create the SwapInts method to swap elements within an array.

---
+++ ArrayHelperMethods/ArrayHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayHelpersLibrary
+{
+    public static class ArrayHelpers
+    {
+        // SwapInts takes in an array and two positions. The method will swap the values for the two.
+        static void SwapInts(int[] arr, int firstIndexPosition, int secondIndexPosition)
+        {
+            // Store the first element's index
+            int tempArr = arr[firstIndexPosition];
+
+            // Assign the first index to the second index
+            arr[firstIndexPosition] = arr[secondIndexPosition];
+
+            // Assigns the second index to the first index temp position
+            arr[secondIndexPosition] = tempArr;
+        }
+    }
+}
